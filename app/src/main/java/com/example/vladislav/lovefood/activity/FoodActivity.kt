cf83ce744d932f19757054ae2d07793b00130b9c
Vladislav <vlad74.rus@mail.ru>
2018-03-08 11:08:42 +0500

    Corrected the activity, removed 2 packages from app/java

--- app/src/main/java/com/example/vladislav/lovefood/activity/FoodActivity.kt
+++ app/src/main/java/com/example/vladislav/lovefood/activity/FoodActivity.kt
@@ -2,7 +2,6 @@ package com.example.vladislav.lovefood.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.Toast
@@ -12,24 +11,21 @@ import com.example.vladislav.lovefood.models.Food
 
 class FoodActivity : AppCompatActivity(){
 
-    private var recyclerView: RecyclerView? = null
+    private lateinit var recyclerView: RecyclerView
 
-    private val foodAdapter = FoodAdapter(Food.food) {
-        foodName -> Toast.makeText(this, "${foodName.nameFood}", Toast.LENGTH_SHORT). show()
+    private val foodAdapter = FoodAdapter(Food.food) {  foodName ->
+        Toast.makeText(this, "${foodName.nameFood}", Toast.LENGTH_SHORT). show()
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_food)
 
         recyclerView = findViewById(R.id.recyclerView)
-        val adapter = foodAdapter
-        val layoutManager = LinearLayoutManager(applicationContext)
-        recyclerView?.layoutManager = layoutManager
-        recyclerView?.itemAnimator = DefaultItemAnimator()
-        recyclerView?.adapter = adapter
-        adapter.notifyDataSetChanged()
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.adapter = foodAdapter
+
+        foodAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/vladislav/lovefood/activity/MainActivity.kt
+++ app/src/main/java/com/example/vladislav/lovefood/activity/MainActivity.kt
@@ -3,36 +3,31 @@ package com.example.vladislav.lovefood.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.Toast
 import com.example.vladislav.lovefood.R
 import com.example.vladislav.lovefood.adapters.RestaurantAdapter
-import com.example.vladislav.lovefood.models.Food
 import com.example.vladislav.lovefood.models.Restaurant
 
 class MainActivity : AppCompatActivity(){
 
-    private var recyclerView: RecyclerView? = null
+    private lateinit var recyclerView: RecyclerView
 
-    private val restaurantAdapter = RestaurantAdapter(Restaurant.restaurants) {
-        restaurant -> onRestaurantClick(restaurant)
+    private val restaurantAdapter = RestaurantAdapter(Restaurant.restaurants) { restaurant ->
+        onRestaurantClick(restaurant)
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recyclerView)
-        val adapter = restaurantAdapter
-        val layoutManager = LinearLayoutManager(applicationContext)
-        recyclerView?.layoutManager = layoutManager
-        recyclerView?.itemAnimator = DefaultItemAnimator()
-        recyclerView?.adapter = adapter
-        adapter.notifyDataSetChanged()
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.adapter = restaurantAdapter
+
+        restaurantAdapter.notifyDataSetChanged()
     }
 
 
